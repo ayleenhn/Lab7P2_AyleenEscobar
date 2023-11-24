abf4045a73b3d734406564b4f5eeeea2b868d5c6
ayleenhn <132100078+ayleenhn@users.noreply.github.com>
2023-11-24 13:24:24 -0600

    Add files via upload
    Commit 1: creacion de clase

---
+++ Lab7P2_AyleenEscobar/src/lab7p2_ayleenescobar/Usuario.java
@@ -0,0 +1,54 @@
+
+package lab7p2_ayleenescobar;
+
+public class Usuario {
+    private String name, user, password;
+    private double Saldo_Compras;
+
+    public Usuario() {
+    }
+
+    public Usuario(String name, String user, String password, double Saldo_Compras) {
+        this.name = name;
+        this.user = user;
+        this.password = password;
+        this.Saldo_Compras = Saldo_Compras;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getUser() {
+        return user;
+    }
+
+    public void setUser(String user) {
+        this.user = user;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public double getSaldo_Compras() {
+        return Saldo_Compras;
+    }
+
+    public void setSaldo_Compras(double Saldo_Compras) {
+        this.Saldo_Compras = Saldo_Compras;
+    }
+
+    @Override
+    public String toString() {
+        return user;
+    }        
+}
